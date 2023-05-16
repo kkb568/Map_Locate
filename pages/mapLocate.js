14252d35fe10f0d6a8b1c0d0c283458d550e22e9
kkb568 <b.koome@alustudent.com>
2023-05-16 11:53:44 +0400

    changes in map locate

--- pages/mapLocate.js
+++ pages/mapLocate.js
@@ -14,24 +14,3 @@ function openMenu() {
 function closeMenu() {
     document.getElementById("menu-content").style.width = "0%";
 }
-
-function openModal(Lat, Long) {
-    var modal = document.getElementById("modal");
-    modal.style.opacity = "1";
-    modal.style.zIndex = "2";
-    
-    var mailForm = document.getElementById("emailSend");
-    const actionArr = mailForm.action.split("/");
-    if (actionArr.length == 5) {
-        mailForm.action += `/${Lat}/${Long}`;
-    } else {
-        mailForm.action = `${actionArr[0]}/${actionArr[1]}/${actionArr[2]}/${actionArr[3]}/${actionArr[4]}`
-        mailForm.action += `/${Lat}/${Long}`;
-    }
-}
-
-function closeModal() {
-    var modal = document.getElementById("modal");
-    modal.style.opacity = "0";
-    modal.style.zIndex = "-1";
-}
